6030d2ef198e0dfcc9097b031a5ff47b85585d33
Sayan Biswas <bsayan289@gmail.com>
2022-08-10 21:21:08 +0530

    using Valiables added

---
+++ usingVariables.java
@@ -0,0 +1,28 @@
+public class usingVariables {
+    public static void main(String[] args)
+    {
+        int myNumber;
+
+        short myShort = 848;
+        long myLong = 9898;
+
+        double myDouble = 8.985345384;
+        float myFloat = 824.8f;
+
+        char myChar = 'O';
+        boolean myBoolean = true;
+
+        byte myByte = 126;
+        //support 8 byte 0 - 127
+
+        myNumber = 88;
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+    }
+}
