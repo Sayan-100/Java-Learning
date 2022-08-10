4192b663a2d4ff11b86f86c3f32bceaf80df5b05
Sayan Biswas <bsayan289@gmail.com>
2022-08-10 21:37:30 +0530

    do strings

---
+++ strings.java
@@ -0,0 +1,22 @@
+public class strings {
+    public static void main(String args[])
+    {
+        int myInt = 8;
+        
+        String text = "Hello";
+
+        String blank = " ";
+        String name = "Tuto";
+
+        String greeting = text + blank + name;
+        
+        System.out.println(greeting);
+
+        System.out.println("Hello" + " " + "Alex");
+        System.out.println("My Integar is : " + myInt);
+
+        double myDouble = 9.8;
+
+        System.out.println("My number is : " + myDouble + ".");
+    }
+}
