8526249939fa7f7da15be0d04f0fae53be5cb4ea
Sayan Biswas <bsayan289@gmail.com>
2022-08-11 21:46:48 +0530

    Learn String Array Java

---
+++ ArrayOfStrings.java
@@ -0,0 +1,34 @@
+public class ArrayOfStrings{
+    public static void main(String[] args)
+    {
+        String[] words = new String[4];
+
+        words[0] = "Hello";
+        words[1] = "and Hi";
+        words[2] = "to";
+        words[3] = "you";
+
+        System.out.println(words[3]);
+
+        String[] fruits = {"apple", "mango", "peer", "kiwi"};
+
+        for(String fruit : fruits)
+        {
+            System.out.println(fruit);
+        }
+
+        int value = 0;
+        
+        String text = null;
+
+        System.out.println(text);
+
+        String []texts = new String[2];
+
+        System.out.println(texts[0]);
+
+
+
+
+    }
+}
